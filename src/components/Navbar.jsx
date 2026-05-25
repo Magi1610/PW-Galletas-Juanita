3c0a4b2c7ad7a30fb4080dfb9c1ed1767010a654
Ivan <martinezgallgosirvingivan@gmail.com>
2026-05-25 13:40:47 -0600

    feat: create navbar component structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Galletas Juanita</h1>
+      <ul>
+        <li>Inicio</li>
+        <li>Nosotros</li>
+        <li>Productos</li>
+        <li>Galería</li>
+        <li>FAQ</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
