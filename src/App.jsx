6ca53da29acda20c67e239d01eb2f278ce580b96
Ivan <martinezgallgosirvingivan@gmail.com>
2026-05-25 17:27:52 -0600

    feat: create products basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero'
 import About from './components/About'
+import Products from './components/Products'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Products />
       <About />
     </>
   );
