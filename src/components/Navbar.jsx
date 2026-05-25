bf265ead8d73d95504adc7336621363d968ab13f
Ivan <martinezgallgosirvingivan@gmail.com>
2026-05-25 14:00:54 -0600

    chore: remove vite defaul styles and configure global css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 function Navbar() {
   return (
-    <nav>
-      <h1>Galletas Juanita</h1>
-      <ul>
-        <li>Inicio</li>
-        <li>Nosotros</li>
-        <li>Productos</li>
-        <li>Galería</li>
-        <li>FAQ</li>
-        <li>Contacto</li>
+    <nav className="navbar">
+      <div className="logo">
+        <h1>Galletas Juanita</h1>
+      </div>
+
+      <ul className="nav-links">
+        <li><a href="#inicio">Inicio</a></li>
+        <li><a href="#nosotros">Nosotros</a></li>
+        <li><a href="#productos">Productos</a></li>
+        <li><a href="#galeria">Galería</a></li>
+        <li><a href="#faq">FAQ</a></li>
+        <li><a href="#contacto">Contacto</a></li>
       </ul>
     </nav>
   );
