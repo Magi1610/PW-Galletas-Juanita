2dcb9cef76095589d46ebe1915c8aef7e506273c
Ivan <martinezgallgosirvingivan@gmail.com>
2026-05-25 21:43:05 -0600

    refactor styles for a responsive app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import '../styles/Navbar.css'
+
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">
+
+      <div className="navbar-logo">
+        <span className="navbar-logo-icon">🍪</span>
         <h1>Galletas Juanita</h1>
       </div>
 
-      <ul className="nav-links">
+      <ul className="navbar-links">
         <li><a href="#inicio">Inicio</a></li>
         <li><a href="#nosotros">Nosotros</a></li>
         <li><a href="#productos">Productos</a></li>
@@ -13,8 +17,11 @@ function Navbar() {
         <li><a href="#faq">FAQ</a></li>
         <li><a href="#contacto">Contacto</a></li>
       </ul>
+
+      <a href="#contacto" className="navbar-cta">Contáctanos</a>
+
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
